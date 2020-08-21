d4142b3f5c9653915e5b22b7e8e13b3a151e5dd8
wei.qi <wei.qi@meri.co>
2020-08-21 16:34:25 +0800

    Group Ranking List Cases

---
+++ src/test/java/com/merico/selenium/test/ee/test/GroupRankingListTest.java
@@ -0,0 +1,80 @@
+package com.merico.selenium.test.ee.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.merico.selenium.page.PageUrlMapping;
+import com.merico.selenium.page.factory.PageFactory;
+import com.merico.selenium.page.support.DefaultUrlNavigator;
+import com.merico.selenium.test.CasePriority;
+import com.merico.selenium.test.TestBase;
+import com.merico.selenium.test.ee.data.TestDataProvider;
+import com.merico.selenium.test.ee.page.LoginPage;
+import com.merico.selenium.test.ee.page.LogoutPage;
+import com.merico.selenium.test.ee.page.RepoReportPage;
+import com.merico.selenium.test.ee.utils.Crypt;
+
+public class GroupRankingListTest extends TestBase {
+protected Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Override
+	protected void initBeforeClass() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@BeforeClass(alwaysRun = true)
+	public void setup() {
+		String targetUrl = PageUrlMapping.getAbsoluteUrl(LoginPage.class);
+		new DefaultUrlNavigator().doNavigation(driver, targetUrl);
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void teardown() {
+		LogoutPage logoutPage = PageFactory.createPage(LogoutPage.class, driver);
+		logoutPage.logout();
+	}
+	
+	@Test(groups = {CasePriority.BVT}, dataProvider = "loginData4AdminProvider", dataProviderClass = TestDataProvider.class, alwaysRun = true)
+	public void testLogin4Admin(String username, String password, Crypt crypt) throws Exception {
+		LoginPage loginPage = PageFactory.createPage(LoginPage.class, driver);
+		loginPage.inputUsernameAndPwd(username, crypt.decrypt(password));
+		loginPage.login4Admin();
+		
+		Assert.assertEquals(username, loginPage.getAccountName(), "Admin login failed!");
+	}
+	
+	@Test(groups = {CasePriority.BVT}, dependsOnMethods = {"testLogin4Admin"}, alwaysRun = true)
+	public void testIntoOverallReport() throws Exception {
+		RepoReportPage repoReportPage = PageFactory.createPage(RepoReportPage.class, driver);
+		repoReportPage.enterIntoOverallReport();
+	}
+	
+	@Test(groups = {CasePriority.BVT}, dependsOnMethods = {"testIntoOverallReport"}, alwaysRun = true)
+	public void testCanvasDisplay() throws Exception {
+		RepoReportPage repoReportPage = PageFactory.createPage(RepoReportPage.class, driver);
+		repoReportPage.canvasDisplay4RankingList();
+		
+		Assert.assertTrue(repoReportPage.getAllCanvasDisplay4RankingList(), "Get All Canvas Display for Ranking List Failed!");
+	}
+	
+	@Test(groups = {CasePriority.BVT}, dependsOnMethods = {"testCanvasDisplay"}, alwaysRun = true)
+	public void testCanvasDisplayByDate() throws Exception {
+		RepoReportPage repoReportPage = PageFactory.createPage(RepoReportPage.class, driver);
+		repoReportPage.canvasDisplayByDate4RankingList();
+		
+		Assert.assertEquals(repoReportPage.getCanvasDisplayMsgByDate4RankingList(), "一些榜单并未显示是因为它们不支持限定时间范围", "Get Canvas Display By Date for Ranking List Failed!");
+	}
+	
+	@Test(groups = {CasePriority.BVT}, dependsOnMethods = {"testCanvasDisplayByDate"}, alwaysRun = true)
+	public void testContributorMainPage() throws Exception {
+		RepoReportPage repoReportPage = PageFactory.createPage(RepoReportPage.class, driver);
+		repoReportPage.contributorMainPage4RankingList();
+		
+		Assert.assertNotEquals(repoReportPage.getContributorInfo4RankingList().length(), 0, "Contributor Main Page Displays for Ranking List Failed!");
+	}
+}
